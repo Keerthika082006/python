45dd8f703eb000174a05eb847030cca93cca76a6
Keerthika <dkeerthika1808@gmail.com>
2025-09-20 20:16:03 +0530

    Add files via upload

---
+++ mathfun.py
@@ -0,0 +1,19 @@
+x=7.49
+y=2
+z=54
+
+result= round(x)
+#round function gives round value eg:1.34 gives 1
+
+result = abs(y)
+#print(result)
+#abs means absolute value abs value of negative value gives positive value
+
+#result = pow(3,2)
+#pow means power eg: 2 power 3 = 8
+
+#result = max(x,y,z)
+#result = min(x,y,z)
+#finding max and min value
+
+print(result)
